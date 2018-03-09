b990cb166a66e7ac0409637acf907fc5b8a28286
NorthernLion <joppetaja@gmail.com>
2018-03-09 12:48:45 +0200

    Init project with hello world and needed folder structure

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+//mongoose.connect(config.mongoUrl)
+//mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(middleware.logger)
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
+})
+
+app.use(middleware.error)
+
+//server.on('close', () => {
+  //mongoose.connection.close()
+//})
+
+module.exports = {
+  app, server
+}
---
+++ stockfront/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class App extends React.Component {
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello World I am the app</p>
+      </div>
+    )
+  }
+}
+
+export default App
